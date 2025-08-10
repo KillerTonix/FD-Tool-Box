8d8107ba5dba8facb7ee7552e7cce90a425f3be0
Artur <Artur@DESKTOP-I9VM6LM>
2025-08-10 11:36:56 +0400

    Add new fonts, UI updates, and localization support
    This commit introduces several enhancements to the application, including the addition of new font resources ("FiraSansBold", "FiraSansRegular", "PTSansBold", "PTSansRegular", "PTSansNarrow-Bold", and "PTSansNarrow-Regular") to improve typography and visual consistency across the UI.
    
    UI elements in `MainWindow.xaml` were renamed for better clarity and updated to utilize the new fonts, enhancing overall readability.
    
    New classes `MaterialsListData` and `SequenceData` were added to manage materials and sequences, respectively, within the `FD_Tool_Box.Utilities.Collections` namespace.
    
    Additionally, localization strings were added to `LanguageFile.json` to support multiple languages, improving accessibility for users.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,9 @@
-﻿using ModernWpf;
-using System.Text;
+﻿using FD_Tool_Box.Utilities.Collections;
+using Microsoft.Win32;
+using ModernWpf;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FD_Tool_Box
 {
@@ -17,15 +12,46 @@ namespace FD_Tool_Box
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<MaterialsListData> MaterialsData { get; set; } = [];
+
         public MainWindow()
         {
             InitializeComponent();
+            MaterialsListView.ItemsSource = MaterialsData;
+            this.DataContext = this;
+            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
 
+        }
+
+        private void AddIntroBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Видеофайлы (*.mp4;*.avi;*.mkv;*.mpeg;*.mpg;*.mov;*.webm;*.ts;*.m4v)| *.mp4; *.avi; *.mkv; *.mpeg; *.mpg; *.mov; *.webm; *.ts; *.m4v",
+                Title = "Выберите Интро",
+                Multiselect = false,
+                CheckFileExists = true,
+                CheckPathExists = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                MaterialsData.Add(new MaterialsListData
+                {
+                    MaterialType = "Интро",
+                    MaterialPath = filePath
+                });
+            }
+            else
+            {
+                
+            }
         }
     }
 }
