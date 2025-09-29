16f0097f4e91a19eada97382419687ffb9a8c64f
Artur <Artur@DESKTOP-I9VM6LM>
2025-09-29 22:40:09 +0400

    Trying to MVVM #2

---
+++ Models/ToolItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FD_Tool_Box.Models
+{
+    public sealed class ToolItem
+    {
+        public string Name { get; set; } = ""; 
+        public object? ViewModel { get; set; }
+    }
+
+}
---
+++ Services/IDialogService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FD_Tool_Box.Services
+{
+    class IDialogService
+    {
+    }
+}
